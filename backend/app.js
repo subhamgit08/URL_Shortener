5a6b83f9f5178fa33e873a15aebe9afc045a6df5
Subham Das <subham08.dev@gmail.com>
2026-08-05 22:06:54 +0530

    minor changes 2

--- backend/app.js
+++ backend/app.js
@@ -37,7 +37,7 @@ app.use(cors({
     credentials: true,
 }));
 
-app.use("/api", express.raw({ type: "application/json" }), webhookRoutes);
+app.use("/api/webhooks", express.raw({ type: "application/json" }), webhookRoutes);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
